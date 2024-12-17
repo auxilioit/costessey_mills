ac435eeaac9428990fe722f9e043fedd76733523
Kyle Sayer <kyle.sayer@auxilio-it.co.uk>
2024-12-17 18:42:52 +0000

    Update status command

---
+++ app/Console/Commands/UpdateLocationStatus.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Illuminate\Support\Carbon;
+
+use App\Models\Report;
+use App\Models\Location;
+
+class UpdateLocationStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'locations:update_status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the status of locations, based on reports in last 24 hours. This is automatically executes every 60 minutes.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $locations = Location::all();
+        foreach($locations as $location){
+            //Update status, based on latested reports in last 24 hours
+            $closed = $location->reports->where('type', 'closed')->where('created_at', '>', Carbon::now()->subHours(24))->count();
+            $flooded = $location->reports->where('type', 'closed')->where('created_at', '>', Carbon::now()->subHours(24))->count();
+            $open = $location->reports->where('type', 'open')->where('created_at', '>', Carbon::now()->subHours(24))->count();
+
+            $largest = max($closed, $flooded, $open);
+
+            if($largest == $closed){
+                $location->status = 'closed';
+                $location->save();
+            }
+            if($largest == $flooded){
+                $location->status = 'flooded';
+                $location->save();
+            }
+            if($largest == $open){
+                $location->status = 'open';
+                $location->save();
+            }
+        }
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
+
+App\Console\Commands\UpdateLocationStatus;
+
+Schedule::command('locations:update_status')->hourly();
